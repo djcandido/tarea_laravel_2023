ce6440bd9a0ced7ee2ca542e1eb8f3cd8833bf48
Jaime Jeovanny Cortez Flores <djcandido@hotmail.com>
2023-06-08 22:44:58 -0600

    Agregada vista para consulta de datos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,8 @@
                     </div>
                     <div class="card-body">
                         <p>En este apartado usted podrá realizar la correspondiente consulta DML de tipo SELECT para obtener y conocer los datos registrados</p>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">Ir a consultar</button>
+                        <!--<button type="button" class="btn btn-lg btn-block btn-primary">Ir a consultar</button>-->
+                        <a href="#" class="btn btn-lg btn-block btn-primary stretched-link">Ir a consultar</a>
                     </div>
                 </div>
                 <div class="card mb-6 shadow-sm">
@@ -15,7 +16,8 @@
                     </div>
                     <div class="card-body">
                         <p>En este apartado, usted podrá realizar las correspondientes inserciones de datos a la tabla, mediante la consulta DML de tipo INSERT</p>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">Ir a Insertar</button>
+                        <!--<button type="button" class="btn btn-lg btn-block btn-primary">Ir a Insertar</button>-->
+                        <a href="#" class="btn btn-lg btn-block btn-primary stretched-link">Ir a insertar</a>
                     </div>
                 </div>
             </div>
@@ -26,7 +28,8 @@
                     </div>
                     <div class="card-body">
                         <p>En este apartado, usted podrá realizar las correspondientes actualización o modificaciones a un registro en particular, mediante la consulta DML UPDATE</p>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">Ir a Actualizar</button>
+                        <!--<button type="button" class="btn btn-lg btn-block btn-primary">Ir a Actualizar</button>-->
+                        <a href="#" class="btn btn-lg btn-block btn-primary stretched-link">Ir a actualizar</a>
                     </div>
                 </div>
                 <div class="card mb-6 shadow-sm">
@@ -35,7 +38,8 @@
                     </div>
                     <div class="card-body">
                         <p>En este apartado, usted podrá realizar las correspondientes eliminaciones de registros a la tabla "proyectos", mediante la consulta DML DELETE</p>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">Ir a Eliminar</button>
+                        <!--<button type="button" class="btn btn-lg btn-block btn-primary">Ir a Eliminar</button>-->
+                        <a href="#" class="btn btn-lg btn-block btn-primary stretched-link">Ir a eliminar</a>
                     </div>
                 </div>
             </div>
